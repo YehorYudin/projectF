db198a189010c72b2e3d2caa93c83e3d74ab5d41
Vlad Asyler <vladomar@gmail.com>
2015-05-16 17:51:24 +0300

    * added user password field * added user login procedure * added email validation * minor changes

--- kachok/models.py
+++ kachok/models.py
@@ -1,9 +1,10 @@
 from mongoengine import *
 from django.conf import settings
 
-connect(settings.DB_CONNECT_STRING)
+connect(settings.X())
 
 class User(Document):
     firstname = StringField(max_length=32, required=True)
     lastname = StringField(max_length=32, required=True)
     email = StringField(max_length=128, required=True)
+    password = StringField(max_length=32, required=True)
